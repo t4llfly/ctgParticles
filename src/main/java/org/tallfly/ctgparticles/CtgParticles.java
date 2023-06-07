43e016a66775a66d26fc0e1c4442177738f68e68
TallFly <tallfly@vk.com>
2023-06-07 22:59:13 +0600

    added type and amount of particles configuration, changed radius of particles calculating.

--- src/main/java/org/tallfly/ctgparticles/CtgParticles.java
+++ src/main/java/org/tallfly/ctgparticles/CtgParticles.java
@@ -5,22 +5,22 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
-import java.util.Objects;
 
 public final class CtgParticles extends JavaPlugin {
     private int particlesCount = 10;
+    private Particle particlesType = Particle.FIREWORKS_SPARK;
     @Override
     public void onEnable() {
         Bukkit.getPluginCommand("setparticles").setExecutor(this);
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, ()-> {
             for(Player player: Bukkit.getOnlinePlayers()){
                 if (player.getWorld().getEnvironment() == World.Environment.THE_END) {
-                    createParticles(player.getLocation().add(0, 8, 0), 10, particlesCount);
-                    createParticles(player.getLocation().add(0, 6, 0), 25, particlesCount);
-                    createParticles(player.getLocation().add(0, 4, 0), 20, particlesCount);
-                    createParticles(player.getLocation().add(0, 2, 0), 15, particlesCount);
-                    createParticles(player.getLocation().add(0, 0, 0), 15, particlesCount);
-                    createParticles(player.getLocation().add(0, -2, 0), 10, particlesCount);
+                    createParticles(player.getLocation().add(0, 8, 0), (int)(Math.random() * 50), particlesCount);
+                    createParticles(player.getLocation().add(0, 6, 0), (int)(Math.random() * 50), particlesCount);
+                    createParticles(player.getLocation().add(0, 4, 0), (int)(Math.random() * 50), particlesCount);
+                    createParticles(player.getLocation().add(0, 2, 0), (int)(Math.random() * 50), particlesCount);
+                    createParticles(player.getLocation().add(0, 0, 0), (int)(Math.random() * 50), particlesCount);
+                    createParticles(player.getLocation().add(0, -2, 0), (int)(Math.random() * 50), particlesCount);
                 }
             }
         }, 20,20);
@@ -31,39 +31,47 @@ public final class CtgParticles extends JavaPlugin {
     public void onDisable() {
     }
 
-    private void createParticles(Location center, int radius, int particles) {
+    private void createParticles(Location center, int radius, int particlesAmount) {
         World world = center.getWorld();
 
-        for (int i = 0; i < particles; i++) {
+        for (int i = 0; i < particlesAmount; i++) {
             double angle = Math.random() * 2 * Math.PI;
             double x = center.getX() + radius * Math.cos(angle);
             double z = center.getZ() + radius * Math.sin(angle);
             double y = center.getY();
 
             Location particleLocation = new Location(world, x, y, z);
-            world.spawnParticle(Particle.FIREWORKS_SPARK, particleLocation, 1, 0,0,0, 0);
+            world.spawnParticle(particlesType, particleLocation, 1, 0,0,0, 0);
         }
     }
 
     @Override
     public boolean onCommand(CommandSender player, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("setparticles")) {
-            if (args.length != 1) {
-                player.sendMessage(ChatColor.RED + "Использование: /setparticles <количество>");
+            if (args.length == 0) {
+                player.sendMessage(ChatColor.RED + "Использование: /setparticles <тип частиц> <количество>");
                 return true;
             }
 
-            int particles;
-            try {
-                particles = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                player.sendMessage(ChatColor.RED + "Некорректное значение для количества частиц.");
-                return true;
+            if (args.length >= 1) {
+                try {
+                    particlesType = Particle.valueOf(args[0].toUpperCase());
+                } catch (Exception e) {
+                    player.sendMessage(ChatColor.RED + "Некорректный тип частиц.");
+                    return true;
+                }
             }
 
-            particlesCount = particles;
+            if (args.length >= 2) {
+                try {
+                    particlesCount = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    player.sendMessage(ChatColor.RED + "Некорректное количество частиц.");
+                    return true;
+                }
+            }
 
-            player.sendMessage(ChatColor.GREEN + "Количество частиц успешно установлено на " + particles + ".");
+            player.sendMessage(ChatColor.GREEN + "Количество частиц " + particlesType + " успешно установлено на " + particlesCount + ".");
             return true;
         }
 
