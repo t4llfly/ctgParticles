9fb1c250bbe95bd82cfb50453b783e9b3e6a6837
TallFly <tallfly@vk.com>
2023-06-08 11:13:08 +0600

    added world configuration and added permission check for /setparticles

--- src/main/java/org/tallfly/ctgparticles/CtgParticles.java
+++ src/main/java/org/tallfly/ctgparticles/CtgParticles.java
@@ -6,15 +6,18 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 
+import static org.bukkit.World.Environment.THE_END;
+
 public final class CtgParticles extends JavaPlugin {
     private int particlesCount = 10;
     private Particle particlesType = Particle.FIREWORKS_SPARK;
+    private World.Environment dimensionType = THE_END;
     @Override
     public void onEnable() {
         Bukkit.getPluginCommand("setparticles").setExecutor(this);
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, ()-> {
             for(Player player: Bukkit.getOnlinePlayers()){
-                if (player.getWorld().getEnvironment() == World.Environment.THE_END) {
+                if (player.getWorld().getEnvironment() == dimensionType) {
                     createParticles(player.getLocation().add(0, 8, 0), (int)(Math.random() * 50), particlesCount);
                     createParticles(player.getLocation().add(0, 6, 0), (int)(Math.random() * 50), particlesCount);
                     createParticles(player.getLocation().add(0, 4, 0), (int)(Math.random() * 50), particlesCount);
@@ -48,31 +51,49 @@ public final class CtgParticles extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender player, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("setparticles")) {
-            if (args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Использование: /setparticles <тип частиц> <количество>");
+            if (!(player instanceof Player)) {
+                player.sendMessage(ChatColor.RED + "Команду можно использовать только в игре.");
                 return true;
             }
 
-            if (args.length >= 1) {
-                try {
-                    particlesType = Particle.valueOf(args[0].toUpperCase());
-                } catch (Exception e) {
-                    player.sendMessage(ChatColor.RED + "Некорректный тип частиц.");
-                    return true;
-                }
+            Player user = (Player) player;
+            if (!user.isOp()) {
+                player.sendMessage(ChatColor.RED + "У вас нет доступа к этой команде.");
+                return true;
+            }
+
+            if (args.length == 0) {
+                player.sendMessage(ChatColor.RED + "Использование: /setparticles [type | amount | setWorld]");
+                return true;
             }
 
             if (args.length >= 2) {
-                try {
-                    particlesCount = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {
-                    player.sendMessage(ChatColor.RED + "Некорректное количество частиц.");
-                    return true;
+                if (args[0].equalsIgnoreCase("type")) {
+                    try {
+                        particlesType = Particle.valueOf(args[1].toUpperCase());
+                        player.sendMessage(ChatColor.GREEN + "Тип частиц изменен на " + particlesType);
+                    } catch (IllegalArgumentException e) {
+                        player.sendMessage(ChatColor.RED + "Некорректный тип частиц.");
+                        return true;
+                    }
+                } else if (args[0].equalsIgnoreCase("amount")){
+                    try {
+                        particlesCount = Integer.parseInt(args[1]);
+                        player.sendMessage(ChatColor.GREEN + "Количество частиц изменено на " + particlesCount);
+                    } catch (NumberFormatException e) {
+                        player.sendMessage(ChatColor.RED + "Некорректное количество частиц.");
+                        return true;
+                    }
+                } else if (args[0].equalsIgnoreCase("setWorld")){
+                    try {
+                        dimensionType = World.Environment.valueOf(args[1].toUpperCase());
+                        player.sendMessage(ChatColor.GREEN + "Мир для появления частиц изменен на " + dimensionType);
+                    } catch (IllegalArgumentException e) {
+                        player.sendMessage(ChatColor.RED + "Некорректное название мира.");
+                        return true;
+                    }
                 }
             }
-
-            player.sendMessage(ChatColor.GREEN + "Количество частиц " + particlesType + " успешно установлено на " + particlesCount + ".");
-            return true;
         }
 
         return false;
